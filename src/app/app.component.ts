38b6d752a996bdbaf202d4dcb50cbc8c7d58a3ed
Alex Hawley <asimo124@yahoo.com>
2019-08-16 00:52:52 -0500

    Finished creating recursive Tree Table.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-table-tree';
+
+  dataset = {
+      fk_Users_2: [
+      {
+        labels: [
+          'First Name',
+          'Last Name'
+        ],
+        vals: [
+          'alex',
+          'hawley'
+        ],
+        oneToManies: [
+          {
+            fk_Posts_2: [
+              {
+                labels: [
+                  'title',
+                  'date'
+                ],
+                vals: [
+                  'Engage2Learn is growing',
+                  '07/12/2019'
+                ],
+                oneToManies: []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        labels: [
+
+        ],
+        vals: [
+          'tom',
+          'smith'
+        ],
+        oneToManies: [
+          /*{
+            fk_Posts_2: [
+              {
+                labels: [
+                  'title',
+                  'date'
+                ],
+                vals: [
+                  'Engage2Learn is growing',
+                  '07/12/2019'
+                ],
+                oneToManies: []
+              }
+            ]
+          }*/
+        ]
+      }
+    ]
+  };
+
+  ngOnInit(): void {
+    console.log('this.dataset: ', this.dataset);
+  }
 }
